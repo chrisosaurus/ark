c602dbf6859050772f2836e03524e7b559370775
Chris Hall <followingthepath@gmail.com>
2012-07-30 21:26:21 +1200

    more structure to configuration and key bindings

--- text.py
+++ text.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 import tkinter
 
-root = tkinter.Tk(className="ark")
-text = tkinter.Text(root, background="black", foreground="white", insertbackground="white")
-text.pack(fill=tkinter.BOTH, expand=1)
-
-text.tag_config("sel", background="yellow")
-text.tag_config("selo", background="red")
+colours = {
+        "bgcolour" : "black", # text area background colour
+        "fgcolour" : "white", # text area 
+        "selcolour" : "yellow", # colour of newest selection
+        "selocolour" : "red", # colour of older selectons
+        "curscolour" : "green", # colour of cursor
+        }
 
 def quit(event):
   root.destroy()
   print( "bye now\n" )
   exit()
 
-# for modifier docu see http://search.cpan.org/~ni-s/Tk-804.027/pod/bind.pod#MODIFIERS
-root.bind("<Control-q>", quit)
-
 def selected(event):
     print("UMMM\n")
 
-# calls selected whenever the selection changes
-text.bind("<<Selection>>", selected)
+# for modifier docu see http://search.cpan.org/~ni-s/Tk-804.027/pod/bind.pod#MODIFIERS
+bindings = {
+        "<<Selection>>" : selected, # calls selected whenever the selection changes
+        "<Control-q>" : quit,
+        }
+
+root = tkinter.Tk(className="ark")
+text = tkinter.Text(root, background=colours["bgcolour"], foreground=colours["fgcolour"], insertbackground=colours["curscolour"])
+text.pack(fill=tkinter.BOTH, expand=1)
+
+text.tag_config("sel", background=colours["selcolour"])
+text.tag_config("selo", background=colours["selocolour"])
+
+for key,value in bindings.items():
+    if key[0] == "<" and key[1] == "<":
+        # special text event
+        text.bind(key, value)
+    else:
+        root.bind(key, value)
+
 root.mainloop()

2169138c3b467263d5f15fff9e631b6915ec4bcf
Chris Hall <followingthepath@gmail.com>
2013-02-16 20:13:14 +1300

    updated TODO after input handling merge, fixed shift backspace case

--- bindings.h
+++ bindings.h
@@ -44,6 +44,7 @@ static Key keys[] = {
 	{ 0,		XK_Up,		f_func,		{ .f=m_prevline } },
 	{ 0,		XK_Down,	f_func,		{ .f=m_nextline } },
 	{ 0,		XK_BackSpace,	f_func,		{ .f=backspace } },
+	{ SHIFT,	XK_BackSpace,	f_func,		{ .f=backspace } },
 
 	{ CTRL,		XK_Left,	f_func,		{ .f=m_prevword } },
 	{ CTRL,		XK_Right,	f_func,		{ .f=m_nextword } }
